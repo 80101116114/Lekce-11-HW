c66d6357b61d6ffd3ed1c837668efec295ba8d92
Petr <bojko.petr2006@gmail.com>
2025-01-03 20:47:43 +0100

    Dokoncen domaci ukol

---
+++ Lekce-11-HW/Product.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Text;
+
+namespace Lekce_11_HW
+{
+
+    internal class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public Product() { }
+
+        public Product(string name, int price, int quantity) 
+        {
+            if (price < 0)
+            {
+                throw new InvalidProductException("Cena nemuze byt zaporna");
+            }
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+        }
+       
+        
+
+
+    }
+}
---
+++ Lekce-11-HW/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Lekce_11_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Catalog catalog = new Catalog();
+
+                catalog.AddProduct(new Product("Notebook ", 1500, 1));
+
+                catalog.AddProduct(new Product("Mobil ", 16000, 8));
+
+
+
+                List<string> serializedProducts = catalog.SerializeProducts();
+                Console.Write("Serializovany list: ");
+                foreach (var json in serializedProducts)
+                {
+                    Console.WriteLine(json);
+                }
+
+                string newProductJson = "{\"Name\":\"Tablet\",\"Price\":5000,\"Quantity\":3}"; // divne ze tady u zadani noveho produktu i po zadani zaporne ceny to vyjimka nechytne
+                List<string> jsonList = new List<string> { newProductJson};
+                catalog.DeserializeProducts(jsonList);
+
+
+                Console.WriteLine("Deserialized: ");
+                foreach (var product in catalog.products)
+                {
+                    Console.WriteLine(product.Name +"cena: "+  product.Price+" pocet kusu: "+ product.Quantity);
+                }
+
+
+                Console.WriteLine("Vsechny produkty");
+                catalog.PrintProducts();
+               
+            }
+            catch (InvalidCastException ex) 
+            {
+                Console.WriteLine("Necekana chyba jemine ");
+            } 
+            catch (InvalidProductException ex)
+            {
+                Console.WriteLine("Chybne zadana cena produktu");
+            }
+            finally 
+            { 
+            Console.WriteLine("Tenhle blok kodu je tak moc cool, ze se vzdy spusti");
+            }
+        }
+      
+        
+
+    }
+}
